3478423627b8b2f5203f7ed56edea9d1e9971674
Reinald Reynoso <reireynoso@gmail.com>
2019-03-13 17:46:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
   contacts.each do |person,data|
     data.each do |attribute,value|
       if attribute == :favorite_icecream_flavors
-        value.delete do |flavor|
+        value.each do |flavor|
           flavor == "strawberry" 
         end
       end
