762ecdbbd3944d6c0674eac101ebb18d98e97d72
Reinald Reynoso <reireynoso@gmail.com>
2019-03-13 17:39:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,16 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+  #contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+  
+  contacts.each do |person,data|
+    data.each do |attribute,value|
+      if attribute == :favorite_icecream_flavors
+        value.each do |flavor|
+          if flavor == "strawberry"
+        end
+      end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
